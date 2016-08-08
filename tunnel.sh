c895b4933896a65e29ee891160f3860318f1194f
Sam Mitchell Finnigan <s.j.finnigan@ncl.ac.uk>
2016-08-08 18:27:32 +0100

    Add optional parameters to allow running tunnel better from command line

--- tunnel.sh
+++ tunnel.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 set +e
-
-SSH_OPTIONS='-nNT -i /opt/tunnel/id_rsa'
-SSH_DOMAIN='sjmf.in'
-SSH_USER='tunnel'
 SSH_SOURCE_PORT=$1		#22
 SSH_REMOTE_PORT=$2		#20000
+SSH_OPTIONS=" -i" ${3:-"/opt/tunnel/id_rsa"}
+SSH_DOMAIN=${4:-'sjmf.in'}
+SSH_USER=${5:-'tunnel'}
+
+# Always assume initial connection will be successful
+export AUTOSSH_GATETIME=0
+
+# Disable echo service, relying on SSH exiting itself
+export AUTOSSH_PORT=0
 
 # Check args
-if [ "$#" -ne 2 ]; then
+if [ "$#" -lt 2 ]; then
     echo "Illegal number of parameters"
 fi
 
-while sleep 5; do
-	ssh $SSH_OPTIONS -gR ${SSH_REMOTE_PORT}:localhost:${SSH_SOURCE_PORT} ${SSH_USER}@${SSH_DOMAIN} 
-done 2> /var/log/user_ssh_error.out
+#to test, use (check out man ssh for explanation of options):
+#autossh -vv -- \
+#	$SSH_OPTIONS -o 'ControlPath none' \
+#	-R ${SSH_REMOTE_PORT}:localhost:22 \
+#	${SSH_USER}@${SSH_DOMAIN} -N \
+#		> /var/user_sshlog.out 2> /var/user_ssh_error.out &
+
+#once proven, use (and comment out previous command):
+autossh -f -- $SSH_OPTIONS -o 'ControlPath none' \
+	-R ${SSH_REMOTE_PORT}:localhost:${SSH_SOURCE_PORT} \
+	${SSH_USER}@${SSH_DOMAIN} -N \
+		2> ${6:-"/var/user_ssh_error.out"}
 
